7c29ef773c1dcc7effe64a63f02f6d03ed951cb6
Paul Gerasymchuk <pgerasymchuk@gmail.com>
2024-05-14 23:24:51 +0300

    Add integration with https://public.nazk.gov.ua/

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-document.getElementById('form').addEventListener('submit', function(event) {
+document.getElementById('form').addEventListener('submit', async function(event) {
 
     event.preventDefault();
     const lastName = document.getElementById('lastName').value;
@@ -7,16 +7,19 @@ document.getElementById('form').addEventListener('submit', function(event) {
 
     clearResults();
 
-    const minNumOfPeople = 1;
-    const maxNumOfPeople = 5;
+    const declarations = await loadDeclarations(lastName + ' ' + firstName + ' ' + patronymicName);
+    const displayed_person_ids = [];
 
-    const numOfPeople = minNumOfPeople + Math.floor(Math.random() * (maxNumOfPeople - minNumOfPeople + 1));
-    for (let i = 0; i < numOfPeople; i++){
-        displayPerson(lastName, firstName, patronymicName);
-    }
+    declarations.forEach(item => {
+        if (!displayed_person_ids.includes(item.personID)){
+            displayed_person_ids.push(item.personID);
+            displayPerson(item.lastName, item.firstName, item.patronymicName, item.personID)
+        }
+    })
 });
 
 document.getElementById('form').addEventListener('reset', clearResults);
+
 
 function clearResults() {
     const results = document.querySelectorAll(`.result`);
@@ -25,80 +28,43 @@ function clearResults() {
     });
 }
 
-function generateDateOfBirth() {
+async function loadDeclarations(query){
 
-    const minAge = 25;
-    const maxAge = 80;
+    const path = `https://public-api.nazk.gov.ua/v2/documents/list?query=${query}`;
 
-    const minDate = new Date();
-    minDate.setFullYear(minDate.getFullYear() - maxAge);
+    const response = await fetch(path);
+    const content = await response.json();
 
-    const maxDate = new Date();
-    maxDate.setFullYear(maxDate.getFullYear() - minAge);
+    const declarations = [];
 
-    const randomDate = new Date(minDate.getTime() + Math.random() * (maxDate.getTime() - minDate.getTime()))
+    content.data.forEach(item => {
+        declarations.push({
+            personID: item.user_declarant_id,
+            declarationID: item.id,
+            date: item.date,
+            lastName: item.data.step_1.data.lastname,
+            firstName: item.data.step_1.data.firstname,
+            patronymicName: item.data.step_1.data.middlename
+        })
+    })
 
-    return `${randomDate.getDate()}.${randomDate.getMonth()+1}.${randomDate.getFullYear()}`;
+    return declarations;
 }
 
-function generatePlaceOfWork() {
-
-    const placesOfWork = [
-        'Міністерство внутрішніх справ України',
-        'Адміністрація Президента України',
-        'Кабінет Міністрів України',
-        'Верховна Рада України',
-        'Міністерство освіти і науки України',
-        'Міністерство економічного розвитку та торгівлі України',
-        'Національний банк України',
-        'Міністерство закордонних справ України',
-        'Генеральна прокуратура України',
-        'Міністерство оборони України'
-    ];
-
-    return placesOfWork[Math.floor(Math.random() * placesOfWork.length)];
-}
-
-function generateRisksMessage() {
-
-    const minNum = 0;
-    const maxNum = 5;
-
-    return minNum + Math.floor(Math.random() * (maxNum - minNum + 1));
-}
-
-function displayPerson(lastName, firstName, patronymicName) {
-
-    const result = document.createElement('div');
-    result.classList.add('result')
+function displayPerson(lastName, firstName, patronymicName, id) {
 
     const h4 = document.createElement('h4');
     h4.innerText = lastName + ' ' + firstName + ' ' + patronymicName;
-    result.appendChild(h4);
 
-    const dob = document.createElement('p');
-    dob.innerText = `Дата народження: ${String(generateDateOfBirth())}`;
-    result.appendChild(dob);
+    const link = document.createElement('a');
+    link.href = `./person.html?id=${id}`;
+    link.target = '_blank';
 
-    const placeOfWork = document.createElement('p');
-    placeOfWork.innerText = String(generatePlaceOfWork());
-    result.appendChild(placeOfWork);
+    const result = document.createElement('div');
+    result.classList.add('result');
 
-    const declaration = document.createElement('a');
-    declaration.innerText = `Декларація за ${(new Date().getFullYear()) - 1} рік`;
-    declaration.href = '#';
-    result.appendChild(declaration);
-
-    const risks = document.createElement('p');
-    const risksNum = generateRisksMessage();
-    if (risksNum){
-        risks.innerText = `Знайдено корупційних ризиків: ${risksNum}.`;
-        result.style.backgroundColor = 'lightpink'
-    } else {
-        risks.innerText = 'Корупційних ризиків не знайдено.';
-        result.style.backgroundColor = 'palegreen';
-    }
-    result.appendChild(risks);
-
+    link.appendChild(h4);
+    result.appendChild(link);
     document.getElementById('container').appendChild(result);
 }
+
---
+++ js/person.js
@@ -0,0 +1,78 @@
+window.onload = async function() {
+    const params = new URLSearchParams(window.location.search);
+    const personID = params.get('id');
+
+    const declarations = await loadPersonDeclarations(personID);
+
+    document.getElementById('lastName-cell').textContent = declarations[0].lastName;
+    document.getElementById('firstName-cell').textContent = declarations[0].firstName;
+    document.getElementById('patronymicName-cell').textContent = declarations[0].patronymicName;
+
+
+    for (let item of declarations){
+        // Річна або виправлена річна декларація
+        if (item.declarationType === 1 || item.declarationType === 3){
+            var d = item;
+            break;
+        }
+    }
+    const detailedDeclaration = await loadDetailedDeclaration(d.declarationID);
+
+    const peopleTable = document.getElementById('related-people-table');
+    const peopleBody = peopleTable.createTBody();
+    detailedDeclaration.data.step_2.data.forEach(item => {
+        const row = peopleBody.insertRow();
+        row.insertCell().textContent = item.subjectRelation;
+        row.insertCell().textContent = item.lastname + ' ' + item.firstname + ' ' + item.middlename
+    })
+
+    const declarationsTable = document.getElementById('declarations-table');
+    const declarationsBody = declarationsTable.createTBody();
+    declarations.forEach(item => {
+        const row = declarationsBody.insertRow();
+        row.insertCell().textContent = new Date(item.date).toLocaleDateString();
+        row.insertCell().textContent = item.year;
+        const cell = row.insertCell();
+        const link = document.createElement('a');
+        link.href = `https://public.nazk.gov.ua/documents/${item.declarationID}`;
+        if (item.declarationType === 2) {
+            link.textContent = 'Повідомлення про суттєві зміни в майновому стані';
+        } else if (item.declarationType === 3) {
+            link.textContent = 'Виправлена декларація';
+        } else {
+            link.textContent = 'Декларація';
+        }
+        cell.appendChild(link);
+    })
+
+}
+
+async function loadPersonDeclarations(personID){
+    const path = `https://public-api.nazk.gov.ua/v2/documents/list?user_declarant_id=${personID}`;
+
+    const response = await fetch(path);
+    const content = await response.json();
+
+    const declarations = [];
+
+    content.data.forEach(item => {
+        declarations.push({
+            declarationID: item.id,
+            declarationType: item.declaration_type,
+            date: item.date,
+            year: item.declaration_year,
+            lastName: item.data.step_1.data.lastname,
+            firstName: item.data.step_1.data.firstname,
+            patronymicName: item.data.step_1.data.middlename,
+        })
+    })
+
+    return declarations;
+}
+
+async function loadDetailedDeclaration(declarationID){
+    const path = `https://public-api.nazk.gov.ua/v2/documents/${declarationID}`;
+    const response = await fetch(path);
+    return await response.json();
+}
+
